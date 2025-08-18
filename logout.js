49fdc3b31242131abc53c2a93b3f43e83a260120
kimyong-jin71 <indra0072@naver.com>
2025-08-18 17:57:38 +0900

    fix(api):user.cjs수정 등

--- logout.js
+++
@@ -1,9 +0,0 @@
-import { withCORS, preflight } from './_cors.js';
-
-export default async function handler(req, res) {
-  if (preflight(req, res)) return;
-  withCORS(res);
-
-  res.setHeader('Set-Cookie', `gh_token=; Path=/; HttpOnly; SameSite=None; Secure; Max-Age=0`);
-  res.status(204).end();
-}
